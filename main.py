8b651c7fcdec62e1d28849d7054fc3a668ce1cc0
Matheus R <matheusrbarros03@gmail.com>
2022-07-27 14:58:15 -0300

    corrigida função dump, modificado texto inicial e adicionado split nos comandos

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
-import fire
 
 import montador
 import dumper
+import loader
+
+# TODO: criar um vetor "memória" interno ao programa
 
 # info: imprime, para cada operação realizada, o conteúdo do registrador; "verbose global"
 # dump: imprime o que está no memoria.txt entre as linhas especificadas
 # load: carrega os dados binários de um arquivo (pode ser o memoria.txt) no vetor interno "memória"
-# TODO: criar um vetor "memória" interno ao programa
 # type: usuário pode digitar assembly livremente na memória interna do programa
 # mount: chama o montador no arquivo especificado e traduz assembly para hexadecimal
 # run: roda o arquivo, enviando-o ao processador (processador é interno ao programa)
@@ -16,7 +17,7 @@ import dumper
 def help():
     print("help, h - Mostra a lista de comandos disponíveis e suas ações")
     print("info, i - Alterna a visibilidade dos elementos de memória e registrador")
-    print("dump [-inicio, -fim]  - Mostra a região de memória de [inicio] a [fim]. Por padrão, inicio = 0 e fim = final do arquivo")
+    print("dump [inicio, fim]  - Mostra a região de memória de [inicio] a [fim]. Por padrão, inicio = 0 e fim = final do arquivo")
     print("load [arquivo] - Carrega os dados presentes em [arquivo]")
     print("type [endereço] - Habilita a escrita na memória a partir de [endereço]")
     print("mount [fonte] [alvo] - Traduz a linguagem de máquina em [fonte] e salva em [alvo]. Por padrão, [alvo] = m_[arquivo]")
@@ -26,8 +27,8 @@ def help():
 def info():
     pass
 
-def dump(arquivo="Memoria.txt", inicio=0, fim=-1):
-    dumper.le_arquivo(arquivo, inicio, fim)
+def dump(inicio=0, fim=-1):
+    dumper.le_memoria(mem_interna, inicio, fim)
 
 comandos = {
     "help": help,
@@ -43,20 +44,20 @@ comandos = {
 }
 
 if __name__ == "__main__":
-    print("Interpretador de assembly v0.1")
+    print("Interpretador de assembly v0.2")
     print("Digite h para uma lista de comandos")
-    print("Pressione ^D para sair")
+    print("Pressione ^D ou ^C para sair")
 
     while True:
         try:
-            comando = input(">>> ")
+            entrada = input(">>> ").split(' ')
 
         except EOFError:
             print()
             sys.exit(0)
 
-        if comando in comandos:
-            comandos[comando]()
+        if entrada[0] in comandos:
+            comandos[entrada[0]]()
         
         else:
             print("Comando não reconhecido.")
