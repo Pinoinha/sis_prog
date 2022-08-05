b392be82289baa6bc70e5b71162e24f49b5ce947
Matheus R <matheusrbarros03@gmail.com>
2022-08-05 17:23:06 -0300

    deletados comentários não mais necessários; help substituído por _help pra evitar ambiguidade; função _help mais descritiva; memória interna substituída por memoria_instrucoes; mount() corrigido; adicionado vetor memoria_dados

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ import dumper
 import loader
 import processador
 
-# info: imprime, para cada operação realizada, o conteúdo do registrador; "verbose global"
-# dump: imprime o que está no memoria.txt entre as linhas especificadas
-# load: carrega os dados binários de um arquivo (pode ser o memoria.txt) no vetor interno "memória"
-# type: usuário pode digitar assembly livremente na memória interna do programa
+## type: usuário pode digitar assembly livremente na memória interna do programa
 # mount: chama o montador no arquivo especificado e traduz assembly para hexadecimal
 # run: roda o arquivo, enviando-o ao processador (processador é interno ao programa)
-# clear: coloca uma string vazia na memoria interna
 
-def help():
+def _help():
     print("help, h - Mostra a lista de comandos disponíveis e suas ações")
     print("info, i - Alterna a visibilidade dos elementos de memória e registrador")
     print("dump [inicio, fim]  - Mostra a região de memória de [inicio] a [fim]. Por padrão, inicio = 0 e fim = final do arquivo")
-    print("load [arquivo] - Carrega os dados presentes em [arquivo]")
+    print("load [arquivo] - Carrega as intruções presentes em [arquivo], por padrão Instrucoes.txt")
+    print("data [arquivo] - Carrega os dados presentes em [arquivo], por padrão Dados.txt")
     print("type [endereço] - Habilita a escrita na memória a partir de [endereço]")
-    print("mount [fonte] [alvo] - Traduz a linguagem de máquina em [fonte] e salva em [alvo]. Por padrão, [alvo] = m_[arquivo]")
+    print("mount [fonte] [alvo] - Traduz as intruções em [fonte] e salva em [alvo]. Por padrão, [alvo] = Montado.txt")
     print("run [endereço] [-s] - Executa o programa escrito a partir de [endereço]. Caso o usuário inclua a opção -s, o programa será executado passo a passo.")
     print("clear [mem/reg/all] - Limpa o conteúdo da memória/acumulador/ambos")
 
@@ -28,39 +25,42 @@ def info():
     quiet = not quiet
 
 def dump(inicio=0, fim=999):
-    global memoria
-    dumper.le_memoria(memoria, inicio, fim)
+    global memoria_instrucoes
+    dumper.le_memoria(memoria_instrucoes, inicio, fim)
 
-#TODO: definir diferenca de load entre instrucoes e dados 
 def load(arquivo_de_instrucoes="Instrucoes.txt"):
-    global memoria
+    global memoria_instrucoes
     global quiet
-    loader.read_and_store(memoria, arquivo_de_instrucoes, quiet)
+    loader.read_and_store(memoria_instrucoes, arquivo_de_instrucoes, quiet)
+
+# TODO: a função deve chamar o loader para salvar na memória interna?
+def data(arquivo_de_dados="Dados.txt"):
+    global memoria_dados
+    global quiet
 
 def type(endereco):
     pass
 
-def mount(arquivo="Instrucoes.txt"):
-    global memoria
-    memoria.clear()
-
-    with open(arquivo) as arq:
-        for linha in arq:
-            hexa = montador.traduzir(str(linha).strip('\r\n'))
-            memoria.append(hexa)
+def mount(fonte="Instrucoes.txt", alvo="Montado.txt"):
+    with open(alvo, 'w') as montado:
+        with open(fonte) as arq:
+            for linha in arq:
+                if '--' not in linha:
+                    hexa = montador.traduzir(str(linha).strip('\r\n'))
+                    montado.write(hexa + '\n')
 
 def run():
     processador.run()
 
 def clear():
-    memoria.clear()
+    memoria_instrucoes.clear()
 
 def exit():
     sys.exit(0)
 
 comandos = {
-    "help": help,
-    "h": help,
+    "help": _help,
+    "h": _help,
     "info": info,
     "i": info,
     "dump": dump,
@@ -79,7 +79,8 @@ if __name__ == "__main__":
     print("Digite h para uma lista de comandos")
     print("Pressione ^D ou ^C para sair")
     
-    memoria = []
+    memoria_instrucoes = []
+    memoria_dados = []
     
     while True:
         try:
