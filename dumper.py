2d2493f5b3ec9a834a02861213293e3565095eaa
Matheus R <matheusrbarros03@gmail.com>
2022-08-05 17:20:23 -0300

    corrigida função le_memoria, retirando a verificação de comentários

--- dumper.py
+++ dumper.py
@@ -9,16 +9,15 @@ def bin_to_hex(num, quiet=True):
         num_hex = hex(int(str(num), base=2))
     except ValueError:
         return hex(int(num, base=16))
-    
+
     if not quiet:
         print(num_hex)
-        
+
     return num_hex
 
 def le_memoria(mem_interna, inicio=0, fim=999):
-    for num_linha, conteudo in enumerate(mem_interna):
-        if (num_linha in range(int(inicio), int(fim)+1)) or (num_linha >= int(inicio) and int(fim)+1 < 0):
-            if '-' in conteudo:
-                num_linha -= 1
-            else:
-                print(num_linha,'-',bin_to_hex(conteudo))
+    for indice, conteudo in enumerate(mem_interna):
+        if (indice in range(int(inicio), int(fim) + 1)) or (
+            indice >= int(inicio) and int(fim) + 1 < 0
+        ):
+            print(indice, "-", bin_to_hex(conteudo))
